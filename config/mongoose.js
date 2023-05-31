9f08630a399fdf23baaa91b779027fdba00348ce
Mohd Faizan <31098815+M0hdF41Z4N@users.noreply.github.com>
2023-05-31 11:04:30 +0530

    Add files via upload

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,13 @@
 // Importing mongoose
 const mongoose = require('mongoose');
-// Connecting to db
-mongoose.connect('mongodb://localhost/tasks_list_db');
-// Checking connection
-const db = mongoose.connection;
-// On Error
-db.on('error',console.error.bind(console,'Error connecting to db'));
-// On successfully running db
-db.once('open',function(){
-    console.log("Successfully connected to the server.");
-});
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/tasks_list_db');
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+// For debugging purpose
+
+// console.log("Successfully Connected to DB");
